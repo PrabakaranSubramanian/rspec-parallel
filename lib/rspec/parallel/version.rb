5898b1a43e7280f4ff073bb0a7a7998ee8d940fe
Prabakaran Subramanian <prabakaran.subramanian@coupa.com>
2017-01-27 16:54:34 -0800

    Update version.rb

--- lib/rspec/parallel/version.rb
+++ lib/rspec/parallel/version.rb
@@ -3,7 +3,8 @@ module RSpec
     # Version information for RSpec Parallel.
     module Version
       # Current version of RSpec Parallel, in semantic versioning format.
-      STRING = '3.0.0.beta2'
+      #STRING = '3.0.0.beta2'
+       STRING = '3.4.0'
     end
   end
 end
